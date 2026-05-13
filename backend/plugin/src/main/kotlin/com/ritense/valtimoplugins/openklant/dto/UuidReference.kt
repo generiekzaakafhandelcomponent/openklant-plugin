1edd1ef5cb92e004a4b19aca8f7b7deaf549c199
Axel Plowman <axel@risa-it.nl>
2026-05-13 17:01:06 +0200

    Replace String with UUID. Should (De)serialize as normal.

--- backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/openklant/dto/UuidReference.kt
+++ backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/openklant/dto/UuidReference.kt
@@ -2,9 +2,10 @@ package com.ritense.valtimoplugins.openklant.dto
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import jakarta.validation.constraints.NotBlank
+import java.util.UUID
 
 data class UuidReference(
     @JsonProperty("uuid")
     @field:NotBlank
-    val uuid: String,
+    val uuid: UUID,
 )
