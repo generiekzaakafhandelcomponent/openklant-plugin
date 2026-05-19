c45e4959de810929fd4c9fa1cb663f67bb8ca4d0
Axel Plowman <axel@risa-it.nl>
2026-05-19 15:43:47 +0200

    Create `DigitaalAdresPatchRequest` DTO

---
+++ backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/openklant/dto/DigitaalAdresPatchRequest.kt
@@ -0,0 +1,24 @@
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.ritense.valtimoplugins.openklant.dto.SoortDigitaalAdres
+import com.ritense.valtimoplugins.openklant.dto.UuidReference
+
+@JsonInclude(JsonInclude.Include.NON_NULL) // Will only include non-null values in patch request
+data class DigitaalAdresPatchRequest(
+    @JsonProperty("verstrektDoorBetrokkene")
+    val verstrektDoorBetrokkene: UuidReference? = null,
+    @JsonProperty("verstrektDoorPartij")
+    val verstrektDoorPartij: UuidReference? = null,
+    @JsonProperty("adres")
+    val adres: String? = null,
+    @JsonProperty("soortDigitaalAdres")
+    val soortDigitaalAdres: SoortDigitaalAdres? = null,
+    @JsonProperty("isStandaardAdres")
+    val isStandaardAdres: Boolean? = null,
+    @JsonProperty("omschrijving")
+    val omschrijving: String? = null,
+    @JsonProperty("referentie")
+    val referentie: String? = null,
+    @JsonProperty("verificatieDatum")
+    val verificatieDatum: String? = null,
+)
