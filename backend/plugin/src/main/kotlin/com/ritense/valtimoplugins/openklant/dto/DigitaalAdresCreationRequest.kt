204d35dae12093b26d896fb1a9cae9af8792ccc2
Axel Plowman <axel@risa-it.nl>
2026-05-20 16:53:56 +0200

    Fix nullability

--- backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/openklant/dto/DigitaalAdresCreationRequest.kt
+++ backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/openklant/dto/DigitaalAdresCreationRequest.kt
@@ -8,7 +8,7 @@ data class DigitaalAdresCreationRequest(
     @JsonProperty("verstrektDoorBetrokkene")
     val verstrektDoorBetrokkene: UuidReference? = null,
     @JsonProperty("verstrektDoorPartij")
-    val verstrektDoorPartij: UuidReference?,
+    val verstrektDoorPartij: UuidReference? = null,
     @JsonProperty("adres")
     val adres: String,
     @JsonProperty("soortDigitaalAdres")
