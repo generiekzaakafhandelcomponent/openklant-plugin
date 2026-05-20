f56bddc83ea5d4029b1870a9c71737762e8e5e73
Axel Plowman <axel@risa-it.nl>
2026-05-20 13:40:35 +0200

    Log warnings separately for paramNames and values

--- backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/openklant/model/DigitaalAdresQuery.kt
+++ backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/openklant/model/DigitaalAdresQuery.kt
@@ -1,16 +1,30 @@
 package com.ritense.valtimoplugins.openklant.model
 
+import mu.KotlinLogging
+
 data class DigitaalAdresQuery(
     val queryParams: MutableMap<String, String> = mutableMapOf()
 ) {
 
     fun add(paramName: String?, value: String?) {
-        if (paramName.isNullOrBlank() || value.isNullOrBlank()) return
+        if (paramName.isNullOrBlank()) {
+            logger.warn { "Did not add paramName-value pair to queryParams: paramName is null or blank" }
+            return
+        }
+
+        if (value.isNullOrBlank()) {
+            logger.warn { "Did not add paramName-value pair to queryParams: value for '$paramName' is null or blank" }
+            return
+        }
 
         if (queryParams.containsKey(paramName)) {
             throw IllegalArgumentException("Duplicate filter key: '$paramName'")
         }
 
         queryParams[paramName] = value
+    }
+
+    companion object {
+        private val logger = KotlinLogging.logger { }
     }
 }
