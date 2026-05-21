df015d03914388feabebcb487b37b1fad194218b
Axel Plowman <axel@risa-it.nl>
2026-05-21 11:29:52 +0200

    Simplify after splitting `UUID` from `UuidReference`

--- backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/openklant/dto/UuidReference.kt
+++ backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/openklant/dto/UuidReference.kt
@@ -1,25 +1,11 @@
 package com.ritense.valtimoplugins.openklant.dto
 
-import com.fasterxml.jackson.annotation.JsonCreator
-import com.fasterxml.jackson.annotation.JsonProperty
-import com.fasterxml.jackson.annotation.JsonValue
-import jakarta.validation.constraints.NotBlank
 import java.util.UUID
 
 data class UuidReference(
     val uuid: UUID,
 ) {
-    @JsonValue
     override fun toString(): String {
         return uuid.toString()
-    }
-
-    companion object {
-        @JvmStatic // Recommended for older versions of Jackson to be able to discover this method in a companion object
-        @JsonCreator
-        fun fromString(uuidString: String): UuidReference{
-            val uuid = UUID.fromString(uuidString)
-            return UuidReference(uuid)
-        }
     }
 }
