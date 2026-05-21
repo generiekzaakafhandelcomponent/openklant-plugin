a546ee4eefa8501e379a5388101dfe6c7d6f8bad
Axel Plowman <axel@risa-it.nl>
2026-05-21 11:10:44 +0200

    Fix Jackson (de)serializing

--- backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/openklant/dto/UuidReference.kt
+++ backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/openklant/dto/UuidReference.kt
@@ -1,19 +1,22 @@
 package com.ritense.valtimoplugins.openklant.dto
 
+import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonValue
 import jakarta.validation.constraints.NotBlank
 import java.util.UUID
 
 data class UuidReference(
-    @JsonProperty("uuid")
-    @field:NotBlank
     val uuid: UUID,
 ) {
+    @JsonValue
     override fun toString(): String {
         return uuid.toString()
     }
 
     companion object {
+        @JvmStatic // Recommended for older versions of Jackson to be able to discover this method in a companion object
+        @JsonCreator
         fun fromString(uuidString: String): UuidReference{
             val uuid = UUID.fromString(uuidString)
             return UuidReference(uuid)
