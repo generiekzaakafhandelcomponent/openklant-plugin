b58349b42ba30fbafa418792cac36ba0c27c595b
Axel Plowman <axel@risa-it.nl>
2026-05-26 14:47:30 +0200

    Increase clarity of warning message.

--- backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/openklant/dto/UuidReference.kt
+++ backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/openklant/dto/UuidReference.kt
@@ -2,16 +2,18 @@ package com.ritense.valtimoplugins.openklant.dto
 
 import java.util.UUID
 
-// Should serialize to an object with one UUID key-value pair, e.g.:
+// Should serialize to an object with one nested UUID key-value pair, e.g.:
 // {
 //     "uuid": "7d0ce1fb-53aa-48bd-be8b-a16727b78843"
 // }
+// This is different from a 'normal' UUID, which gets serialized like this:
+// "uuid": "7d0ce1fb-53aa-48bd-be8b-a16727b78843"
+// So NOT in an object.
+
 data class UuidReference(
     val uuid: UUID,
 ) {
-    override fun toString(): String {
-        return uuid.toString()
-    }
+    override fun toString(): String = uuid.toString()
 
     companion object {
         fun fromString(uuidString: String): UuidReference {
