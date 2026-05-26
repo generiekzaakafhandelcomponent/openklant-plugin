53733280f2dad22214b3ff3562e9b51b14724d76
Axel Plowman <axel@risa-it.nl>
2026-05-26 14:49:00 +0200

    Remove important notice

--- backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/openklant/jackson/StringToBooleanDeserializer.kt
+++ backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/openklant/jackson/StringToBooleanDeserializer.kt
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonParser
 import com.fasterxml.jackson.databind.DeserializationContext
 import com.fasterxml.jackson.databind.JsonDeserializer
 
-// Also known as the FormioDeStupidifier
 class StringToBooleanDeserializer : JsonDeserializer<Boolean?>() {
     override fun deserialize(
         p: JsonParser,
