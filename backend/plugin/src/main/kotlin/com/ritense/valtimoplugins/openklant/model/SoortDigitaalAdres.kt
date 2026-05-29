60e470d04a22ac04a87c2afde8180bb1b4a8d910
Jasper Kunkeler <jasper@risa-it.nl>
2026-05-29 13:25:17 +0200

    Make enum so it can read proper by jackson

--- backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/openklant/model/SoortDigitaalAdres.kt
+++ backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/openklant/model/SoortDigitaalAdres.kt
@@ -1,16 +1,14 @@
 package com.ritense.valtimoplugins.openklant.model
 
-import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonValue
 
 enum class SoortDigitaalAdres(
     val value: String,
 ) {
-    @JsonProperty("email")
     EMAIL("email"),
-
-    @JsonProperty("telefoonnummer")
     TELEFOONNUMMER("telefoonnummer"),
+    OVERIG("overig");
 
-    @JsonProperty("overig")
-    OVERIG("overig"),
+    @JsonValue
+    fun toJson() = value
 }
