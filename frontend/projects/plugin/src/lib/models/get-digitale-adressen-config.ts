87a8ed436ccb32b05f93598cc2ce57a2f4d7fa85
Jasper Kunkeler <jasper@risa-it.nl>
2026-05-29 10:06:19 +0200

    Change queryparams to string type in the frontend.

--- frontend/projects/plugin/src/lib/models/get-digitale-adressen-config.ts
+++ frontend/projects/plugin/src/lib/models/get-digitale-adressen-config.ts
@@ -1,9 +1,4 @@
 export interface GetDigitaleAdressenConfig {
     resultPvName: string;
-    queryParams: KeyValueQueryParam[];
-}
-
-interface KeyValueQueryParam {
-    key: string;
-    value: string;
+    queryParams: string;
 }
