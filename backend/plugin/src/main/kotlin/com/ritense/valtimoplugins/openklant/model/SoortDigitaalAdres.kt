f2295dcc3b897acee2558e60aa6fe7efb46816ae
Jasper Kunkeler <jasper@risa-it.nl>
2026-05-29 13:36:07 +0200

    Reintroduce JsonProperty so it can be read properly

--- backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/openklant/model/SoortDigitaalAdres.kt
+++ backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/openklant/model/SoortDigitaalAdres.kt
@@ -1,12 +1,18 @@
 package com.ritense.valtimoplugins.openklant.model
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonValue
 
 enum class SoortDigitaalAdres(
     val value: String,
 ) {
+    @JsonProperty("email")
     EMAIL("email"),
+
+    @JsonProperty("telefoonnummer")
     TELEFOONNUMMER("telefoonnummer"),
+
+    @JsonProperty("overig")
     OVERIG("overig");
 
     @JsonValue
